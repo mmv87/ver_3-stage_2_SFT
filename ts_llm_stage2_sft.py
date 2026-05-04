4eb8aeb3827f79f8fbc751a9eb605e1131b25210
mithun_vmk <mithun.kct@gmail.com>
2026-05-03 22:46:42 -0400

    second

--- ts_llm_stage2_sft.py
+++ ts_llm_stage2_sft.py
@@ -36,7 +36,7 @@ tokenizer.add_special_tokens(special_token_dict)"""
 import json
 _json_file = os.path.join(os.environ["SLURM_TMPDIR"],"sft_train.jsonl")
 ###datapipeline
-dataset=ts_textual(21,5,tokenizer_modified,_json_file,17500,device=device)
+dataset=ts_textual(21,5,tokenizer_modified,_json_file,15000,device=device)
 dataloader=DataLoader(dataset,batch_size=1,shuffle=True,collate_fn=lambda b:collate_func(b,tokenizer=tokenizer_modified))
 """
 dataset= ts_multimodal_text(128,128,_json_file,tokenizer,device=device,model_dtype=None)
